029417a3004fca1a702bdd816cad4e448d1eb7cf
Ralph02 <info@ralphmedia.co.uk>
2018-10-11 10:26:54 +0100

    added addon.setup.php for EE4

---
+++ system/expressionengine/third_party/pg/addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+return [
+  'author'         => 'Caddis (TJ Draper)',
+  'author_url'     => 'http://www.caddis.co',
+  'name'           => 'PG',
+  'description'    => 'Fetch a single request value or loop through all GET/POST parameters.',
+  'version'        => '1.2.0',
+  'namespace'      => 'caddis/pg',
+  'settings_exist' => FALSE,
+];
